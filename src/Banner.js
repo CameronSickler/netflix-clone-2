8e2a3ca154ee78c89953495eddcadd81836ccb04
Cameron <sicklecj@gmail.com>
2023-02-21 19:06:49 -0600

    add API and axios js

--- src/Banner.js
+++ src/Banner.js
@@ -2,10 +2,29 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
-    return (
-        <div className="banner">
 
-        </div>
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+    }
+
+    return (
+        <header className="banner" style={{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://wallpapercave.com/wp/wp8741529.jpg")`,
+            backgroundPosition: "center center",
+        }}>
+
+            <div className="banner_contents">
+                <h1 className="banner_title">Movie Name</h1>
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+                <h1 className="banner_description">{truncate(`This is a test description`, 150)}</h1>
+            </div>
+
+            <div className="banner--fadeBottom" />
+        </header >
     )
 }
 
