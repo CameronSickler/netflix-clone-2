ad154cf6ae616c44a322a0b9e3eb481fe112b64c
Cameron <sicklecj@gmail.com>
2023-02-23 16:50:57 -0600

    add row js

--- src/Banner.js
+++ src/Banner.js
@@ -35,17 +35,17 @@ function Banner() {
     return (
         <header className="banner" style={{
             backgroundSize: "cover",
-            backgroundImage: `url("https://wallpapercave.com/wp/wp8741529.jpg")`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center",
         }}>
 
             <div className="banner_contents">
-                <h1 className="banner_title">Movie Name</h1>
+                <h1 className="banner_title">{movie?.title || movie?.name || movie?.original_name}</h1>
                 <div className="banner_buttons">
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My List</button>
                 </div>
-                <h1 className="banner_description">{truncate(`This is a test description`, 150)}</h1>
+                <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
             </div>
 
             <div className="banner--fadeBottom" />
