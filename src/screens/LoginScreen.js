6e86316135a8ef9d7d65d91bdadfef554e1bf4ae
Cameron <sicklecj@gmail.com>
2023-02-24 17:30:00 -0600

    style loginscreen form input

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -15,6 +15,24 @@ function LoginScreen() {
                 </button>
 
                 <div className='loginScreen__gradient' />
+
+                <div className="loginScreen__body">
+                    <>
+                        <h1>Unlimited films, TV programs and more.</h1>
+                        <h2>Watch anywhere. Cancel at anytime.</h2>
+                        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+
+                        <div className="loginScreen__input">
+                            <form>
+                                <input type='email' placeholder='Email Address' />
+                                <button className='loginScreen__getStarted'>
+                                    GET STARTED
+                                </button>
+                            </form>
+                        </div>
+                    </>
+                </div>
+
             </div>
         </div>
     )
