ba8653d2511186ce57b521c3dfbafdcd4960d3d5
Cameron <sicklecj@gmail.com>
2023-03-09 11:56:06 -0600

    practice with promiseAll

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -11,11 +11,28 @@ function PlansScreen() {
 
             const prodRef = collection(db, 'products');
             const prodSnap = await getDocs(prodRef);
+            // const products = {};
 
             prodSnap.forEach(async (prodDoc) => {
+
+                // const priceRef = collection(db, 'products', products[prodDoc.id], "prices");
+                // const priceSnap = await getDocs(priceRef);
+
+                // priceSnap.forEach(async (priceDoc) => {
+                //     products[prodDoc.id] = priceDoc.data();
+                // })
+
+                // first go into prices
+                // second grab prices data
+                // third add prices data product document
+                //fourth create objects for each product document
+
                 products[prodDoc.id] = prodDoc.data();
 
             });
+
+
+
             setProducts(products);
         };
         get();
@@ -31,3 +48,18 @@ function PlansScreen() {
 }
 
 export default PlansScreen
+
+
+//debugging attempt to use Promise.all() to map over for paths
+
+  // const paths = await Promise.all(prodSnap.map(async (prodDoc) => {
+            //     const priceRef = collection(db, "products", prodDoc.data().id, "prices");
+            //     const priceSnap = await getDocs(priceRef);
+
+            //     return priceSnap.docs.map((priceDoc) => {
+            //         params: {
+            //             id: `${priceDoc.data().priceID}`,
+            //                 price: `${priceDoc.data().price}`,
+            //         }
+            //     })
+            // }))
