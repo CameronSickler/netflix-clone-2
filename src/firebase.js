07e63fec043d0bf32bd9ec0225cc38a5ce0abe9d
Cameron <sicklecj@gmail.com>
2023-03-16 15:49:47 -0500

    add commented out blocks for modular 9 working code in firebase js and plansscreen js prepping for 9 compat refactor

--- src/firebase.js
+++ src/firebase.js
@@ -24,3 +24,34 @@ export {
 }
 
 export default db;
+
+
+
+
+//WORKING CODE IN MODULAR 9
+// import { initializeApp } from "firebase/app";
+// import { getFirestore, collection, getDocs } from "firebase/firestore";
+// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID
+// };
+
+// const firebaseApp = initializeApp(firebaseConfig);
+// const db = getFirestore(firebaseApp);
+// const auth = getAuth(firebaseApp);
+
+// export {
+//     auth,
+//     createUserWithEmailAndPassword,
+//     signInWithEmailAndPassword,
+//     collection,
+//     getDocs
+// }
+
+// export default db;
--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -41,3 +41,52 @@ function PlansScreen() {
 }
 
 export default PlansScreen
+
+
+
+
+
+//WORKING CODE IN MODULAR 9
+// import React, { useState, useEffect } from 'react';
+// import db, { collection, getDocs } from "../firebase";
+// import './PlansScreen.css';
+
+// function PlansScreen() {
+//     const [products, setProducts] = useState([]);
+
+//     useEffect(() => {
+//         const getProd = async () => {
+//             const prodRef = collection(db, 'products');
+//             const prodSnap = await getDocs(prodRef);
+
+//             prodSnap.forEach((prodDoc) => {
+
+//                 products[prodDoc.id] = prodDoc.data();
+
+
+//                 // Retrieve prices for this product
+//                 // doc.ref.collection('prices').get().then((querySnapshot) => {
+//                 //     querySnapshot.forEach((doc) => {
+//                 //         const price = doc.data();
+//                 //         price.id = doc.id;
+//                 //         product.prices.push(price);
+//                 //     });
+
+//                 //     products.push(product);
+//                 //     setProducts(products);
+//                 // })
+//             });
+//             setProducts(products);
+//         };
+//         getProd();
+//     }, [products]);
+//     console.log(products)
+
+//     return (
+//         <div className='plansScreen'>
+
+//         </div>
+//     )
+// }
+
+// export default PlansScreen
